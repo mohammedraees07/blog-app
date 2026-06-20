37a09367f368265f4cfc06c28c19b36205027642
Mohammed Raees <mohammedraees004@gmail.com>
2026-06-21 00:08:38 +0530

    change password route

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,104 +1,137 @@
-import User from "../models/User.js"
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
 
-export const registerUser = async(req,res)=>{
-    try {
-        const {username , email, password, role} = req.body
+    const checkExistingUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
 
-        const checkExistingUser = await User.findOne({ $or : [{username},{email}]})
-
-        if(checkExistingUser){
-            return res.status(409).json({
-                success : false,
-                message : 'User already exists with this username or email. try using different username or email'
-            })
-        }
-
-        const hashedPassword = await bcrypt.hash(password,10)
-
-        const newlyCreatedUser = new User(
-            {
-            username,
-            email,
-            password : hashedPassword,
-            role : role || "user"
-            }
-        )
-
-        await newlyCreatedUser.save()
-
-       
-            return res.status(201).json(
-                {
-                    success : true,
-                    message : 'user registered successfully',
-                }
-            )
-        
-
-
-    } catch (error) {
-        return res.status(500).json({
-            success : false,
-            message : 'something went wrong! please try again'
-        })
+    if (checkExistingUser) {
+      return res.status(409).json({
+        success: false,
+        message:
+          "User already exists with this username or email. try using different username or email",
+      });
     }
-}
 
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    const newlyCreatedUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      role: role || "user",
+    });
 
+    await newlyCreatedUser.save();
 
-export const loginUser = async(req,res)=>{
-    try {
-        
-        const {username , password} = req.body
+    return res.status(201).json({
+      success: true,
+      message: "user registered successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "something went wrong! please try again",
+    });
+  }
+};
 
-        const user = await User.findOne({username})
+export const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-        if(!user){
-            return res.status(404).json({
-                success : false,
-                message : 'User not found with this username. please register to login'
-            })
-        }
+    const user = await User.findOne({ username });
 
-        const isPasswordMatch = await bcrypt.compare(password, user.password)
-
-        if(!isPasswordMatch){
-            return res.status(400).json({
-                success : false,
-                message : 'Invalid credentials. Please enter valid credentials'
-            })
-        }
-
-        if(!process.env.JWT_SECRET_KEY){
-            throw new Error("JWT secret missing");
-        }
-
-        const accessToken = jwt.sign(
-            {
-                userId : user._id,
-                username : user.username,
-                role: user.role
-            },process.env.JWT_SECRET_KEY,{
-                expiresIn : '20m'
-            }
-        )
-
-        return res.status(200).json({
-            success : true,
-            message : 'Logged in successfully',
-            accessToken
-        })
-
-
-
-    } catch (error) {
-         return res.status(500).json({
-            success : false,
-            message : 'something went wrong! please try again'
-        })
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found with this username. please register to login",
+      });
     }
-}
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials. Please enter valid credentials",
+      });
+    }
+
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error("JWT secret missing");
+    }
+
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+        username: user.username,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "20m",
+      },
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      accessToken,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "something went wrong! please try again",
+    });
+  }
+};
+
+export const changePassword = async (req, res) => {
+  try {
+    const userId = req.userInfo.userId;
+
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(userId);
+
+    // we check for user existence bcz there is a possibilty that user is dltd by admin but we still got 10 more mins for jwt to expire so check for existence is needed
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+
+    if (!oldPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Old password is incorrect! Please try again.",
+      });
+    }
+
+    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = newHashedPassword;
+
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Password changed Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+        success : false,
+        message : 'Something went wrong! Please try again.'
+    })
+  }
+};
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { loginUser, registerUser } from "../controllers/auth-controller.js"
+import { changePassword, loginUser, registerUser } from "../controllers/auth-controller.js"
+import authMiddleware from "../middleware/auth-middleware.js"
 
 
 const router = express.Router()
@@ -7,5 +8,6 @@ const router = express.Router()
 
 router.post('/login',loginUser)
 router.post('/register',registerUser)
+router.patch('/change-password',authMiddleware,changePassword)
 
 export default router
